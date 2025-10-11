1373f28164b4abca692e158d20376fbebae55ffc
ntr0pie <tripathi.shreeshv@gmail.com>
2025-10-11 15:16:56 -0400

    revamp workplace component

--- src/routes/resume/Workplace.svelte
+++ src/routes/resume/Workplace.svelte
@@ -12,22 +12,25 @@
 </script>
 
 <div
-  class="mb-8"
+  class="mb-8 border-b border-neutral-300 pb-8"
+  style="border-bottom-width: 0.5px;"
   class:cursor-pointer={hidden}
   on:click={() => (hidden = false)}
   on:keypress={() => {}}
 >
   <h3 class="text-black text-lg leading-tight mb-1">
-    {title} at <a class="link" href={url}>{company}</a>
+    <span class="font-medium">{company}</span>
+    <span class="px-0.5 text-neutral-500 text-lg">•</span>
+    <span class="opacity-90">{title}</span>
   </h3>
   <p class="font-light mb-2">
     <span class="whitespace-nowrap">{dates}</span>
-    <span class="px-0.5 text-neutral-500">•</span>
+    <span class="px-0.5 text-neutral-500 text-lg">•</span>
     <span class="whitespace-nowrap">{location}</span>
   </p>
   {#if !hidden}
-    <ul class="list-disc pl-7 marker:text-neutral-400" in:fade|local>
+    <div class="opacity-85" in:fade|local>
       <slot />
-    </ul>
+    </div>
   {/if}
 </div>
