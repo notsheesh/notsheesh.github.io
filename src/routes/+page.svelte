0eb4678d84a039807b8db47a4b939acf7f211700
ntr0pie <tripathi.shreeshv@gmail.com>
2025-10-11 15:17:35 -0400

    minor change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
   <span class="neutral">is a</span>
   silicon engineer<span class="neutral">,</span><br />
   programmer<span class="neutral">, and</span><br />
-  researcher<span class="neutral">.</span>
+  confused human<span class="neutral">.</span>
 </p>
 
 <div class="layout-md text-md md:text-lg space-y-14">
